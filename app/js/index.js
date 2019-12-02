44590c21b5f58b55c52e5cba085173597b80e2e8
xaander1 <alexnjogu52@gmail.com>
2019-12-02 17:08:40 +0300

    Change tabs handler to es6

--- app/js/index.js
+++ app/js/index.js
@@ -1,16 +1,15 @@
-//ES7 asynchronous functions
+//ES6 javascript promises
 const { Tabs } = require ('./src/tabsrouter.js');
-async function renderTab() {
-	try{
-		let defaultTab = await new Tabs(null,'header-tab','sidebar','./sidebar/default.htm');
-	}
-	catch(err){
-		console.log(err);
-	}
-}
-(async function render() {
-	renderTab();
-	//default route
+new Tabs(null,'header-tab','sidebar','./sidebar/default.htm')
+.then(()=>{
 	new Tabs(null,'nav-group-item','main','./mainview/default.htm');
+})
+.then(()=>{
 
-})();
+})
+.catch(error=>console.log(error));
+
+function listenForClicks() {
+	
+
+}
