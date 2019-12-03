e5af547b808d6b86c8f488ae6c158b61ab71ae05
xaander1 <alexnjogu52@gmail.com>
2019-12-03 16:54:13 +0300

    Nested routing and other bug fixes

--- app/js/index.js
+++ app/js/index.js
@@ -1,15 +1,16 @@
 //ES6 javascript promises
-const { Tabs } = require ('./src/tabsrouter.js');
-new Tabs(null,'header-tab','sidebar','./sidebar/default.htm')
-.then(()=>{
-	new Tabs(null,'nav-group-item','main','./mainview/default.htm');
+
+//nested default tab
+const { Tabs,listenForClicks,listenForNestedClicks } = require ('./src/tabsrouter.js');
+ Tabs(null,'sidebar','./sidebar/home.htm')
+.then((reply)=>{
+	if(reply=='Success'){
+	Tabs(null,'main','./mainview/home/alexander.htm');
+	}
 })
 .then(()=>{
-
+	listenForNestedClicks('.header-tab','alexander.htm','.fav');
+	//listen for click events on sidebar
+	listenForClicks('.fav','home');
 })
 .catch(error=>console.log(error));
-
-function listenForClicks() {
-	
-
-}
--- app/src/tabsrouter.js
+++ app/src/tabsrouter.js
@@ -1,61 +1,81 @@
-class Tabs{
-    constructor(evt,tabslinks,section,page_to_load,){
-    return new Promise(
-      (res,rej)=>{
-        this.page_to_load = page_to_load;
-        this.tabslinks = tabslinks;
-        this.evt=evt;
-        this.section=section;
-        let status_log = this.tabsHandler();
-        res(status_log);
+const Tabs = (commonlink,section,page_to_load)=>{
+  return new Promise(function(resolve, reject) {
+          fetch(page_to_load)
+          .then(data => data.text())
+          .then(html=> {
+          document.getElementById(section).innerHTML = html;
+          var scripts = document.getElementById(section).querySelectorAll("script");
+          for (var i = 0; i < scripts.length; i++) {
+              if (scripts[i].innerText) {
+                  eval(scripts[i].innerText);
+              } else {
+              fetch(scripts[i].src)
+              .then(data => { data.text()
+              .then(r=> eval(r))});
       }
-    )
-
+      // To not repeat the element
+      scripts[i].parentNode.removeChild(scripts[i]);
     }
-//handle the routing between pages
-    tabsHandler(){
-        fetch(this.page_to_load)
-        .then(data => data.text())
-        .then(html=> {
-        document.getElementById(this.section).innerHTML = html;
-        var scripts = document.getElementById(this.section).querySelectorAll("script");
-        for (var i = 0; i < scripts.length; i++) {
-            if (scripts[i].innerText) {
-                eval(scripts[i].innerText);
-            } else {
-            fetch(scripts[i].src)
-            .then(data => { data.text()
-            .then(r=> eval(r))});
-
+  }).then(()=>{
+    if(!commonlink){
+      resolve('Success')
+    }else{
+      let i,tablinks;
+          tablinks = document.querySelectorAll(commonlink);
+          //console.log(evt);
+          for (i = 0; i < tablinks.length; i++) {
+              tablinks[i].className = tablinks[i].className.replace(" active", "");
+          }
+           resolve('Success')
     }
-    // To not repeat the element
-    scripts[i].parentNode.removeChild(scripts[i]);
-  }
-  if(this.event){
-  let state = this.classChanger();
-  return state;
-  }else{
-    return "success";
-  }  
-});
-
-    }
-
-//Change class  to active
-  classChanger(){
-    let i,tablinks;
-     tablinks = document.getElementsByClassName(this.tabslinks);
-     for (i = 0; i < tablinks.length; i++) {
-         tablinks[i].className = tablinks[i].className.replace(" active", "");
-     }
-      this.evt.currentTarget.className += " active";
-      return "success";
-  }
-
-
-
-
+  });
+  });
 }
+
+
+function listenForClicks(classX,folder) {
+	let tags = document.querySelectorAll(classX);
+	tags.forEach(tag=>{
+		tag.onclick=event=>{
+			 //console.log(event.currentTarget.className);
+			//console.log(tablinks = document.querySelectorAll('fav'));
+			Tabs(classX,'main','./mainview/'+folder+'/'+tag.id+'.htm')
+			.then(reply=>{
+				if(reply=='Success'){
+				document.getElementById(tag.id).className += " active";
+				}
+			});
+		}
+	});
+}
+
+
+function listenForNestedClicks(commonClass,defaultHtml,commonClass2){
+	let headtags = document.querySelectorAll(commonClass);
+	headtags.forEach(headtag=>{
+			headtag.onclick=event=>{
+				Tabs(commonClass,'sidebar','./sidebar/'+headtag.id+'.htm')
+			 .then((reply)=>{
+				 if(reply=='Success'){
+				 Tabs(null,'main','./mainview/'+headtag.id+'/'+defaultHtml);
+				 }
+			 })
+			 .then(()=>{
+						document.getElementById(headtag.id).className += " active";
+			 })
+			 .then(()=>{
+			 //listen for click events on sidebar
+			 listenForClicks(commonClass2,headtag.id);
+			 })
+			 .catch(error=>console.log(error));
+			}
+	});
+}
+
+
+
 module.exports = {
-  Tabs
+  Tabs,
+  listenForClicks,
+  listenForNestedClicks
 }
